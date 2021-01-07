3a218d7e8e553dd22a81d7217e4c09c02b49b828
Caroline Araujo <ca.araujosouza@gmail.com>
2021-01-07 15:31:30 -0300

    modelando as collections

--- src/br/com/alura/TestaCursoComAluno.java
+++ src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
@@ -18,12 +21,25 @@ public class TestaCursoComAluno {
 		javaColecoes.matricula(a2);
 		javaColecoes.matricula(a3);
 		
-		System.out.println("Todos os alunos matriculados nesse curso: ");
+		System.out.println("Todos os alunos matriculados: ");
 		javaColecoes.getAlunos().forEach(a -> {
 			System.out.println(a);
 		});
 		
 		System.out.println("O aluno " + a1 + " está matriculado?");
+		
+		Set<Aluno> alunos = javaColecoes.getAlunos();
+		Iterator<Aluno> iterador = alunos.iterator();
+		while(iterador.hasNext()) {
+			Aluno proximo = iterador.next();
+			System.out.println(proximo);
+		}
+
+		
+//		for(Aluno a : javaColecoes.getAlunos()) {
+//			System.out.println(a);
+//		}
+		
 		System.out.println(javaColecoes.estaMatriculado(a1));
 		
 		
