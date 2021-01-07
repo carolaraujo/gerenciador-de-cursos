5f6fef35696be0c30be790f6f5f0c381aab248ea
Caroline Araujo <ca.araujosouza@gmail.com>
2021-01-07 14:30:30 -0300

    trocando List por Set

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -3,8 +3,10 @@ package br.com.alura;
 
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 
 public class Curso {
@@ -12,6 +14,8 @@ public class Curso {
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
+	
 	
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -41,6 +45,19 @@ public class Curso {
 	@Override
 	public String toString() {
 		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", aulas: " + this.aulas + "]";
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+		
+	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+
+	public boolean estaMatriculado(Aluno aluno) {
+		return this.alunos.contains(aluno);
 	}
 
 }
